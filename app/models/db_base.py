ddf3ad26fab0dcdfe38c27347215d41845feb096
Jose Laruta <eduardo.laruta@gmail.com>
2021-01-01 19:41:17 -0400

    added timestamp base

--- app/models/db_base.py
+++ app/models/db_base.py
@@ -3,8 +3,16 @@ from uuid import UUID, uuid4
 from pydantic import BaseModel, Field
 
 
-class DBBase(BaseModel):
-    """Default Database fields"""
-    id: UUID = Field(default_factory=uuid4)
+class TimestampDBBase(BaseModel):
+    """Base model with timestamps for database"""
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+
+class DBBase(TimestampDBBase):
+    """Default Database fields"""
+    id: UUID = Field(default_factory=uuid4)
+
+
+
+
