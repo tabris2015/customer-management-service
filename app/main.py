b49cd1fb717a41307a4a88a9402d5ebfd703e673
Jose Laruta <eduardo.laruta@gmail.com>
2021-01-03 01:47:02 -0400

    added cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 import os                           # para obtener variables de entorno
 import uvicorn                      # servidor ASGI para correr fastapi
 from fastapi import FastAPI         # modulo principal para el servicio web
+from fastapi.middleware.cors import CORSMiddleware
 import app.settings as settings
 from .routers import todo, transaction, client           # rutas
 
 app = FastAPI(title=f'To-do app: {settings.TITLE}', version=settings.VERSION)
 
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+    )
 # routers
 app.include_router(todo.router, tags=['To-do'], prefix='/todos')
 app.include_router(transaction.router, tags=['Transactions'], prefix='/transactions')
