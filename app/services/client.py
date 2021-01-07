bd7aab92451236f799e39f1750c6a5caf6001c0e
Jose Laruta <eduardo.laruta@gmail.com>
2021-01-06 22:54:01 -0400

    added methods to client service for fetch a client only once

--- app/services/client.py
+++ app/services/client.py
@@ -1,8 +1,13 @@
 from typing import List
 from app.daos.client import ClientDAO
 from app.models.client import Client, ClientCreate, ClientUpdate
+from app.models.account import Account, AccountCreate, AccountUpdate
+from app.services.account import AccountService
+from app.daos.account import AccountDAO
 
 client_dao = ClientDAO()
+account_dao = AccountDAO()
+account_service = AccountService()
 
 
 class ClientService:
@@ -20,3 +25,19 @@ class ClientService:
 
     def delete_client(self, id: str) -> None:
         return client_dao.delete(id)
+
+    def list_accounts(self, id: str) -> List[Account]:
+        client = self.get_client(id)
+        return client.accounts
+
+    def get_account(self, id: str, account_id: str):
+        client = self.get_client(id)
+        for account in client.accounts:
+            if account.id == account_id:
+                return account
+
+    def create_account(self, account_create: AccountCreate):
+        return account_service.create_account(account_create)
+
+    def update_account(self, account_update: AccountUpdate):
+        return account_dao.update(account_update.client_id, account_update.id, account_update)
