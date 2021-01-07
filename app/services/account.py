8bf4cdcc35f072ddfddaa983645e37857fc658a8
Jose Laruta <eduardo.laruta@gmail.com>
2021-01-06 20:38:44 -0400

    added client object to account service

--- app/services/account.py
+++ app/services/account.py
@@ -1,6 +1,7 @@
 from typing import List
 from app.daos.account import AccountDAO
 from app.models.account import Account, AccountCreate, AccountUpdate
+from app.models.client import Client
 
 account_dao = AccountDAO()
 
@@ -9,14 +10,14 @@ class AccountService:
     def create_account(self, account_create: AccountCreate) -> Account:
         return account_dao.create(account_create)
 
-    def get_account(self, client_id: str, id: str) -> Account:
-        return account_dao.get(client_id, id)
+    def get_account(self, client: Client, id: str) -> Account:
+        return account_dao.get(client.id, id)
 
-    def list_accounts(self, client_id: str) -> List[Account]:
-        return account_dao.list(client_id)
+    def list_accounts(self, client: Client) -> List[Account]:
+        return account_dao.list(client.id)
 
-    def update_account(self, client_id: str, id: str, account_update: AccountUpdate) -> Account:
-        return account_dao.update(client_id, id, account_update)
+    def update_account(self, client: Client, id: str, account_update: AccountUpdate) -> Account:
+        return account_dao.update(client.id, id, account_update)
 
-    def delete_account(self, client_id: str, id: str) -> None:
-        return account_dao.delete(client_id, id)
+    def delete_account(self, client: Client, id: str) -> None:
+        return account_dao.delete(client.id, id)
